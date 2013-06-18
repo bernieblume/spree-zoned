b09baf7b3afbaff4ef5a349698416d6a9cb2d1ad
LiuSihao <liusihao123@qq.com>
2013-06-18 17:43:15 +0800

    fix products index rows

--- app/overrides/add_admin_products_index_rows.rb
+++ app/overrides/add_admin_products_index_rows.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(
-  :name             => "replace_index_rows",
+  :name             => "add_admin_products_index_rows",
   :virtual_path     => "spree/admin/products/index",
-  :replace_contents => "[data-hook='admin_products_index_rows']",
+  :insert_after => "[data-hook='admin_products_index_row_actions']",
   :partial          => "spree/admin/zoned/index_rows",
 )
