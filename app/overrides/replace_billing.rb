8d7a9a55b9b01e974968f23d39dbb26b8af7e3fe
raykin <raykincoldxiao@gmail.com>
2013-07-26 13:51:05 +0800

    remove billing override. It was move to the app cause it's not belongs to the Gem

--- app/overrides/replace_billing.rb
+++
@@ -1,6 +0,0 @@
-Deface::Override.new(
-  :name             => "replace_billing",
-  :virtual_path     => "spree/checkout/_address",
-  :replace_contents => "[data-hook='billing_inner']",
-  :partial          => "spree/zoned/billing",
-)
