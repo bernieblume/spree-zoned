1f197ba12426e6ceacc1217fc46fd16b68471dd5
raykin <raykincoldxiao@gmail.com>
2013-08-12 15:49:27 +0800

    fix issue#1320. checkout blocked on delivery

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -4,7 +4,11 @@ module Spree
     attr_accessor :accept
     attr_accessible :accept
 
-    validates :accept, :presence => true, :if => :ship_address_id?
+    validates :accept, :presence => true, :if => :bill_address_id_updated
+
+    def bill_address_id_updated
+      changed.include? "bill_address_id"
+    end
 
   end
 end
